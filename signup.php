d519153ee8ac06b8fa6ad56a5131a1e7e2d93ff1
Adnan Sharaf <149807745+AdnanSharaf@users.noreply.github.com>
2025-09-15 20:44:52 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,42 @@
+<?php
+$servername = "localhost";
+$username = "root"; // update if needed
+$password = ""; // update with your MySQL password
+$dbname = "college_db";
+
+// Connect to DB
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form values
+$name = $_POST['name'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+$gender = $_POST['gender'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+
+// Check password match
+if ($password !== $confirm_password) {
+  die("Passwords do not match!");
+}
+
+// Hash password
+$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+// Insert into DB
+$sql = "INSERT INTO users (name, email, number, gender, password) 
+        VALUES ('$name', '$email', '$number', '$gender', '$hashed_password')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Signup successful!";
+} else {
+  echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
